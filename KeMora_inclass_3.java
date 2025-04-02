ad50c77e0509d11b0dec82bed473f745539915ba
PamothKumarasinghe <nikolailaazz2@gmail.com>
2025-04-02 09:10:27 +0530

    func

--- KeMora_inclass_3.java
+++ KeMora_inclass_3.java
@@ -1,5 +1,21 @@
+/** Functional : 
+ * Display study room information
+ * reserve a study room
+ * release a study room
+ * concurrency handling
+ * simulated student actions
+ * error handling and exception
+ * 
+ * multiple test runs
+ */
+/** Non-Functionaldsf
+ * concurrency management
+ * execution requirments
+ */
+
+
 public class KeMora_inclass_3 {
     public static void main() {
-        
+
     }
 }
